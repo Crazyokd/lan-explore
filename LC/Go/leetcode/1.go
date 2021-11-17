eda2cbdde9181d583cbd9125711e227833eed2bd
Rekord <1324596506@qq.com>
2021-11-17 13:46:09 +0800

    第二题

--- LC/Go/leetcode/1.go
+++ LC/Go/leetcode/1.go
@@ -23,3 +23,30 @@ func twoSum(nums []int, target int) []int {
 func TwoSum(nums []int, target int) []int {
 	return twoSum(nums,target)
 }
+
+type ListNode struct {
+    Val int
+	Next *ListNode
+}
+func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	head := &ListNode{Val: 0}
+	n1,n2,carry,current := 0,0,0,head
+	for l1 != nil || l2 != nil || carry != 0{
+		if l1 != nil{
+			 n1=l1.Val
+			 l1=l1.Next
+		 }else{
+			 n1=0
+		 }
+		 if l2 != nil{
+			 n2=l2.Val
+			 l2=l2.Next
+		 }else{
+			 n2=0
+		 }
+		 current.Next = &ListNode{Val: (n1+n2+carry)%10}
+		 current = current.Next
+		 carry = (n1+n2+carry)/10
+	}
+	return head.Next
+}
--- LC/c++/1.cpp
+++ LC/c++/1.cpp
@@ -7,6 +7,13 @@ int main(){
     vector<int> ans(2);//创建一个size=2的vector
     return 0;
 }
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -23,4 +30,27 @@ public:
         }
         return ans;
     }
+
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *head = new ListNode();
+        int n1=0,n2=0,carry=0;
+        ListNode *current=head;
+        while(l1!=nullptr || l2 != nullptr || carry != 0){
+            if(l1==nullptr)n1=0;
+            else{
+                n1=l1->val;
+                l1=l1->next;
+            }
+            if(l2==nullptr)n2=0;
+            else{
+                n2=l2->val;
+                l2=l2->next;
+            }
+            current->next=new ListNode((n1+n2+carry)%10);
+            current=current->next;
+            carry=(n1+n2+carry)/10;
+        }
+        return head->next;
+    }
+
 };
