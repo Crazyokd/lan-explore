7bad6a603161e74c8a2b9a2b3d10e641a1c9b08b
Rekord <1324596506@qq.com>
2021-12-20 20:07:29 +0800

    将CMS从lan-explore迁移至CMS仓库

--- Python/shop_project/util/set_font.py
+++
@@ -1,9 +0,0 @@
-import dearpygui.dearpygui as dpg
-
-def set_font():
-    with dpg.font_registry(): #注册字体，自选字体
-        with dpg.font("fonts/STKAITI.ttf", 20) as font_cn:	#增加中文编码范围，防止问号
-            dpg.add_font_range_hint(dpg.mvFontRangeHint_Default)
-            dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Simplified_Common)
-            dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Full)
-    dpg.bind_font(font_cn)
