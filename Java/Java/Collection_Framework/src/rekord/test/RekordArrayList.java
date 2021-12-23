c212335fb6bdc833e4d92b9329a5d38d44c1dc72
Rekord <1324596506@qq.com>
2021-12-23 11:52:27 +0800

    begin Java Collection Framework

---
+++ Java/Java/Collection_Framework/src/rekord/test/RekordArrayList.java
@@ -0,0 +1,5 @@
+package rekord.test;
+
+public class RekordArrayList {
+    
+}
---
+++ Java/Java/Collection_Framework/src/rekord/test/Test.java
@@ -0,0 +1,14 @@
+package rekord.test;
+
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args){
+        List al = new ArrayList();
+        List ll = new LinkedList<>();
+        Map hm = new HashMap();
+        Map lhm = new LinkedHashMap();
+        Hashtable ht = new Hashtable();
+    }
+
+}
