b15918bfc38727b8c7993d5901cd0322d060f096
Rekord <1324596506@qq.com>
2022-01-15 13:01:41 +0800

    learn dynamic method invocation.

---
+++ Java/struts2/struts2-3/src/main/java/oct/rekord/struts2/TestDynamicMethodInvocationActoin.java
@@ -0,0 +1,15 @@
+package oct.rekord.struts2;
+
+public class TestDynamicMethodInvocationActoin {
+
+	public String save(){
+		System.out.println("save...");
+		return "success";
+	}
+	
+	public String update(){
+		System.out.println("update...");
+		return "success";
+	}
+	
+}
