60b336e1854199e68c324a5a148d26714e1c769b
Rekord <1324596506@qq.com>
2022-01-17 17:24:00 +0800

    Spring继承Junit4和Junit5

---
+++ Java/Spring/spring5_txdemo1/src/oct/rekord/spring5/test/JTest5.java
@@ -0,0 +1,24 @@
+package oct.rekord.spring5.test;
+
+import oct.rekord.spring5.service.UserService;
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration("classpath:bean1.xml")
+
+@SpringJUnitConfig(locations = "classpath:bean1.xml")
+public class JTest5 {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void test1() {
+        userService.accountMoney();
+    }
+}
