0f97ec3438b7cc5952be8a985780fe7c3b59da6c
Rekord <1324596506@qq.com>
2022-03-28 11:30:24 +0800

    perf: perf code structrue and print format

--- C++/word_analyzer.cpp
+++ C++/word_analyzer.cpp
@@ -31,8 +31,15 @@ vector<string> noIdentifier = { "true", "false", "null" };
 // 分界符
 vector<string> limiterWords = { ".", ",", ";", "[", "]", "{", "}", "(", ")", ":" };
 
+const int keyWordBenchmark = 0;
+const int arithmeticBenchmark = keyWordBenchmark + keyWords.size();
+const int relationBenchmark = arithmeticBenchmark + arithmeticOperations.size();
+const int bitBenchmark = relationBenchmark + relationOperations.size();
+const int logicBenchmark = bitBenchmark + bitOperations.size();
+const int limiterBenchmark = logicBenchmark + logicOperations.size();
 
-// 判断是否为数字【暂不处理进制相关】
+
+// 判断是否为数字
 bool isNumber(string word) {
     if (word[0] < '0' || word[0] > '9') {
         return false;
@@ -70,20 +77,22 @@ bool isNumber(string word) {
 
 
 //
-bool isSpecial(vector<string>& list, string word) {
+bool isSpecial(vector<string>& list, string word, int& index) {
     for (int i = 0; i < list.size(); i++) {
         if (list[i] == word) {
+            index = i;
             return true;
         }
     }
     return false;
 }
 
+
 void detachlimiter(string& word) {
     for (int i = 0; i < word.size(); i++) {
         for (int j = 0; j < limiterWords.size(); j++) {
             if (word[i] == limiterWords[j][0]) {
-                cout << word[i] << " ";
+                cout << "delimiter\t" << limiterBenchmark + j << "\t" << word[i] << endl;
                 word = word.substr(0, i) + " " + word.substr(i + 1);
                 i--;
                 break;
@@ -93,33 +102,31 @@ void detachlimiter(string& word) {
 }
 
 // 分析word
-void analyzeWord(string word) {
-    if (isSpecial(keyWords, word) ) {
-        cout << "keyword==> ";
-        return;
+int analyzeWord(string word) {
+    int index = -1;
+    if (isSpecial(keyWords, word, index) ) {
+        cout << "keyword";
+        index = keyWordBenchmark + index;
+    }else if (isSpecial(arithmeticOperations, word, index)) {
+        cout << "ariOpe";
+        index = arithmeticBenchmark + index;
+    }else if (isSpecial(relationOperations, word, index)) {
+        cout << "relaOpe";
+        index = relationBenchmark + index;
+    }else if (isSpecial(bitOperations, word, index)) {
+        cout << "bitOpe";
+        index = bitBenchmark + index;
+    }else if (isSpecial(logicOperations, word, index)) {
+        cout << "logicOpe";
+        index = logicBenchmark + index;
+    }else if (isNumber(word)) {
+        index = -2;
+        cout << "number";
+    }else {
+        cout << "id";
     }
-    if (isSpecial(arithmeticOperations, word)) {
-        cout << "arithmeticOperation==> ";
-        return;
-    }
-    if (isSpecial(relationOperations, word)) {
-        cout << "relationOperation==> ";
-        return;
-    }
-    if (isSpecial(bitOperations, word)) {
-        cout << "bitOperation==> ";
-        return;
-    }
-    if (isSpecial(logicOperations, word)) {
-        cout << "logicOperation==> ";
-        return;
-    }
-    if (isNumber(word)) {
-        cout << "number==> ";
-        return;
-    }
-    
-    cout << "identifier==> ";
+    cout << "\t\t";
+    return index;
 }
 
 
@@ -157,16 +164,18 @@ bool handleComment(string& word, bool comment) {
 }
 
 
+// 抽取字符串常量【优化：将返回值改为 int 】
 bool handleString(string& word, string delimiter) {
     int index_front = word.find(delimiter);
     if (index_front >= 0) {
         int index_back_pre = index_front + 1;
         int index_back = word.find(delimiter, index_back_pre);
+        // fix the bugs about escape character
         while(index_back > index_back_pre && word[index_back - 1] == '\\') {
             index_back_pre = index_back + 1;
             index_back = word.find(delimiter, index_back_pre);
         }
-        cout << word.substr(index_front, index_back - index_front + 1) << " ";
+        cout << "string\t\t-3\t" << word.substr(index_front, index_back - index_front + 1) << endl;
         word = word.substr(0, index_front) + " " + word.substr(index_back + 1);
         return true;
     }
@@ -183,11 +192,9 @@ void analyzeByLine(vector<string> lines) {
         comment = handleComment(line, comment);
         // 处理字符串常量
         while(handleString(line, "\"") || handleString(line, "'"));
-        cout << endl;
 
         // 分离分界符
         detachlimiter(line);
-        cout << endl;
 
         char *cstr = new char[line.length() + 1];
         strcpy(cstr, line.c_str());
@@ -197,8 +204,7 @@ void analyzeByLine(vector<string> lines) {
         while (p != NULL) {
             string word(p);
 
-            analyzeWord(word);
-            cout << word << endl;
+            cout << analyzeWord(word) << "\t" << word << endl;
             p=strtok(NULL, " ");
         }
         delete[] cstr;
