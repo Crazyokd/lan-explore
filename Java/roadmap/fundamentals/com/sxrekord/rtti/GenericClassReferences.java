9707c3cd35f99ccf7aa4f4e1888bb4bf11a9c2ac
Rekord <1324596506@qq.com>
2022-06-24 21:08:32 +0800

    feat: reseach and test the Java RTTI

---
+++ Java/roadmap/fundamentals/com/sxrekord/rtti/GenericClassReferences.java
@@ -0,0 +1,13 @@
+//: typeinfo/GenericClassReferences.java
+package com.sxrekord.rtti; /* Added by Eclipse.py */
+
+public class GenericClassReferences {
+    public static void main(String[] args) {
+        Class intClass = int.class;
+        Class<Integer> genericIntClass = int.class;
+        genericIntClass = Integer.class; // Same thing
+        intClass = double.class;
+//         genericIntClass = double.class; // Illegal
+    }
+}
+///:~
---
+++ Java/roadmap/fundamentals/com/sxrekord/rtti/reflect/A.java
@@ -0,0 +1,6 @@
+//: typeinfo/interfacea/A.java
+package com.sxrekord.rtti.reflect;
+
+public interface A {
+    void f();
+} ///:~
